ed90f34e863f415f811b4fc75b48fe38cf042e8e
student <student@students-mac.local>
2019-08-10 10:30:44 +0200

    Initial Commit

---
+++ MobileAppsIOSWerkstuk1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MobileAppsIOSWerkstuk1
+//
+//  Created by student on 10/08/2019.
+//  Copyright © 2019 EhB. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MobileAppsIOSWerkstuk1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MobileAppsIOSWerkstuk1
+//
+//  Created by student on 10/08/2019.
+//  Copyright © 2019 EhB. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
