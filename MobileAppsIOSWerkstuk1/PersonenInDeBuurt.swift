dd5cc3014ae721816235a0a882ca9da5799768b1
student <student@students-Mac.local>
2019-08-16 17:45:02 +0200

    Implemented Personen in de buurt

--- MobileAppsIOSWerkstuk1/PersonenInDeBuurt.swift
+++ MobileAppsIOSWerkstuk1/PersonenInDeBuurt.swift
@@ -1,25 +1,41 @@
-//
-//  SecondViewController.swift
-//  MobileAppsIOSWerkstuk1
-//
-//  Created by student on 10/08/2019.
-//  Copyright © 2019 EhB. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class PersonenInDeBuurt: UIViewController {
-
+class PersonenInDeBuurt: UIViewController, CLLocationManagerDelegate {
+    let data = Personen().lijst
+    let manager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        for persoon in data {
+            let annotation = MKPointAnnotation()
+            let coordinate = CLLocationCoordinate2D(latitude: persoon.coordinaten.breedtegraad, longitude: persoon.coordinaten.lengtegraad)
+            
+            annotation.title = "\(persoon.voornaam) \(persoon.familienaam)"
+            annotation.subtitle = "\(persoon.adres.straat) \(persoon.adres.huisnummer), \(persoon.adres.postcode) \(persoon.adres.plaats)"
+            annotation.coordinate = coordinate
+            
+            map.addAnnotation(annotation)
+
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0]
+        let userCoordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
+        let span = MKCoordinateSpanMake(100.0,100.0)
+        let region = MKCoordinateRegionMake(userCoordinate, span)
+        map.showsUserLocation = true
+        map.setRegion(region, animated: true)
     }
-
-
 }
 
